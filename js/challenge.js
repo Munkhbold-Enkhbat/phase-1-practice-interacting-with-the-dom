be9cfd14a1d9925bb4f4afa6ed8072550bb28760
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-08 12:19:13 -0800

    Completed Assignment

--- js/challenge.js
+++ js/challenge.js
@@ -44,3 +44,34 @@ pause.addEventListener('click', () => {
     startTimer = setInterval(timerSet, 1000)
   }
 })
+
+let objects = []
+
+heart.addEventListener('click', () => {
+  const h1 = document.querySelector('#counter')
+  const ul = document.querySelector('ul')
+  const li = document.createElement('li')
+  let num = parseInt(h1.innerText)
+  let currentLi = document.getElementById(h1.innerText)
+  
+  if(currentLi === null) {
+    li.innerHTML = `${num} has been liked <span>1</span> time`
+    li.id = num
+    li.className = '1'
+    ul.appendChild(li)
+  } else if(currentLi) {
+    debugger    
+    let timesClicked = parseInt(currentLi.className) + 1
+    currentLi.className = timesClicked
+    currentLi.innerText = `${currentLi.id} has been liked ${timesClicked} times`
+  }
+})
+
+document.querySelector('button#submit').addEventListener('click', (e) => {
+  e.preventDefault()
+  let submittedContent = document.querySelector('input#comment-input').value
+  let p = document.createElement('p')
+  p.innerText = submittedContent
+
+  document.getElementById('list').appendChild(p)
+})
